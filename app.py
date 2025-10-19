db63c1d8b1e01ca9904a43f1b3232e4e6255878d
angelosegarra-code <angelo.segarra@gmail.com>
2025-10-19 13:54:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -115,3 +115,6 @@ def shop():
 def contact():
     return render_template('contact.html')
 if __name__=='__main__': app.run(host='0.0.0.0', port=8080)
+@app.route('/about')
+def about():
+    return render_template('about.html')
