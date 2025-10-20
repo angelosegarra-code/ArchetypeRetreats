cc946b6242d924df85d6a143287e84991bb0abda
angelosegarra-code <angelo.segarra@gmail.com>
2025-10-20 13:35:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -114,7 +114,10 @@ def shop():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
-if __name__=='__main__': app.run(host='0.0.0.0', port=8080)
-@app.route('/about')
+    @app.route('/thank-you')
+def thank_you():
+    return render_template('thank_you.html')
+    @app.route('/about')
 def about():
     return render_template('about.html')
+if __name__=='__main__': app.run(host='0.0.0.0', port=8080)
